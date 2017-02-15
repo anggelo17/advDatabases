efcbe04cf81d5c81b8ad7ff09c79ba99c7111a1c
supriyahazarath <supriya@s172-20-198-h22.paws.uga.edu>
2017-02-15 14:26:09 -0500

    2PL with weight for graph

--- src/main/scala/trans/LockTable.scala
+++ src/main/scala/trans/LockTable.scala
@@ -2,72 +2,72 @@ package trans
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
 import scala.collection.mutable
-<<<<<<< HEAD
-import scala.collection.mutable.HashMap
-import scala.collection.mutable.ListMap
-=======
 import Cycle._
->>>>>>> origin/master
 /**
   * Created by supriya on 2/9/17.
   */
 object LockTable {
-<<<<<<< HEAD
-  class Lock {
-    val lock = new ReentrantReadWriteLock()
-  }
-  var lockmap = new mutable.HashMap[Int,Lock]()
-  //lockmap += 0-> new Lock()
-
-  def rl(oid: Int) =
-  {
-    if(!(lockmap.get(oid)==null))
-      lockmap += (oid -> new Lock())
-    lockmap(oid).lock.readLock().lock()
-    println("======="+lockmap(oid).lock.toString)
-  }
-  def wl(oid: Int) = {
-    if (!lockmap.keySet.contains(oid))
-      lockmap += (oid) -> new Lock()
-    lockmap(oid).lock.writeLock().lock()
-    println("======="+lockmap(oid).lock.toString)
-  }
-  def ul(oid: Int): Unit = {
-    lockmap(oid).lock.writeLock().unlock()
-  }
-}
-=======
 class Lock(tid: Int) {
   val lock = new ReentrantReadWriteLock()
-  val transid=tid
+  val owner=tid
 }
   var lockmap = new mutable.HashMap[Int,Lock]()
+  val nTrans = 5
+  val waitgraph = new Graph(Array.fill(nTrans)(Set[Int]()))
 
-  def rl(oid: Int,tid: Int) =
+  def rl(oid: Int,tid: Int,trans: Transaction) =
   {
-    if(!(lockmap.keySet.contains(oid))) {
+    var ownerTid = -1
+    var wait =false
+    if(!(lockmap.keySet.contains(oid)))
       lockmap += (oid -> new Lock(tid))
-      lockmap(oid).lock.readLock().lock()
-    }
-    else {
-     if(checkCycle(3,tid,lockmap(oid).transid))
+      //lockmap(oid).lock.readLock().lock()
 
-    }
+    if(tid != lockmap(oid).owner)
+      wait = true
+    ownerTid = lockmap(oid).owner
+    if(wait) { waitgraph.ch(tid) += ownerTid}
+
+    if(LockTable.checkCycle())
+      {
+        trans.rollbackid = true
+        if(wait) waitgraph.ch(tid) -= ownerTid
+      }
+    lockmap(oid).lock.readLock().lock() //tid got the readlock
+
+
     //println("======="+lockmap(oid).lock.toString)
   }
-  def wl(oid: Int,tid: Int) = {
+  def wl(oid: Int,tid: Int,trans: Transaction) = {
+    var wait = false
+    var ownerTid = -1
     if (!lockmap.keySet.contains(oid))
       lockmap += (oid) -> new Lock(tid)
+    if(lockmap(oid).owner != tid)
+      wait = true
+    ownerTid = lockmap(oid).owner
+    if(wait)
+      {
+        waitgraph.ch(tid) += ownerTid
+      }
+    if(LockTable.checkCycle())
+      {
+        trans.rollbackid=true
+        if(wait)
+        waitgraph.ch(tid) -= ownerTid
+      }
     lockmap(oid).lock.writeLock().lock()
     //println("======="+lockmap(oid).lock.toString)
   }
-    def ul(oid: Int): Unit = {
-      lockmap(oid).lock.writeLock().unlock()
+    def ul(op: Char,oid: Int): Unit = {
+      if(lockmap.keySet.contains(oid)) {
+        if (op=='w')
+          lockmap(oid).lock.writeLock().unlock()
+        else
+          lockmap(oid).lock.readLock().unlock()
+      }
     }
-   def checkCycle(noTrans: Int,tid1 :Int,tid2: Int): Boolean ={
-   val g=new Graph(Array.fill(noTrans) (Set[Int]()))
-     g.ch(tid1) += tid2
-     hasCycle(g)
+   def checkCycle(): Boolean ={
+     hasCycle(waitgraph)
    }
   }
->>>>>>> origin/master
--- src/main/scala/trans/Transaction2pl.scala
+++ src/main/scala/trans/Transaction2pl.scala
@@ -1,196 +1,3 @@
-<<<<<<< HEAD
-//
-////::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-///** @author  John Miller
-//  *  @version 1.1
-//  *  @date    Tue Jan 10 14:34:43 EST 2017
-//  *  @see     LICENSE (MIT style license file).
-//  *------------------------------------------------------------------------------
-//  *  Instructions:
-//  *      Download sbt
-//  *      Download transactions.zip
-//  *      unzip transactions.zip
-//  *      cd transactions
-//  *      sbt
-//  *      > compile
-//  *      > run-main trans.ScheduleTest
-//  *      > exit
-//  */
-//
-//package trans
-//
-//import java.util.concurrent.locks.ReentrantReadWriteLock
-//
-//import Operation._
-//
-////::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-///** The `Transaction` companion object
-//  */
-//
-//object TransactionStats {
-//  var start: Long = System.currentTimeMillis
-//  var end: Long = 0
-//  var count: Int = 0 // successful transactions
-//  var rolls: Int = 0
-//}
-//object Transaction
-//{
-//  private var count = -1
-//
-//  def nextCount () = { count += 1; count }
-//
-//
-//  VDB.initCache ()
-//
-//} // Transaction object
-//
-//import Transaction._
-//import LockTable._
-//
-////::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-///** The `Transaction` class
-//  *  @param sch  the schedule/order of operations for this transaction.
-//  */
-//class Transaction (sch: Schedule,protocol: Int) extends Thread
-//{
-//  private val DEBUG = true              // debug flag
-//private val tid = nextCount ()        // tarnsaction identifier
-//val twoPl=2
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Run this transaction by executing its operations.
-//    */
-//  override def run ()
-//  {
-//    begin ()
-//    for (i <- sch) {
-//      val op = i
-//      val oid=i._3
-//      println (i)
-//      if (op._1 == r)
-//      {
-//        if(protocol==twoPl) {
-//          if(searchlock(oid,sch)){
-//            wl(oid)
-//           // read(oid)
-//          }
-//          else {
-//            rl(op._3)
-//            //read(op._3)
-//            lockmap(oid).lock.readLock().unlock() // ul(op._3)
-//          }
-//        } //if
-//      } //if r/w
-//      else {
-//        if (protocol == twoPl) {
-//          wl(op._3)
-//          write(op._3, VDB.str2record(op.toString))
-//        }
-//      }//else of r/w
-//    } // for
-//    commit ()
-//  } // run
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Read the record with the given 'oid'.
-//    *  @param oid  the object/record being read
-//    */
-//  def read (oid: Int): VDB.Record =
-//  {
-//    //obtain lock. see if there is a write ahead, so the lock should be upgraded - searchlock
-//    VDB.read (tid, oid)._1
-//  } // read
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Write the record with the given 'oid'.
-//    *  @param oid    the object/record being written
-//    *  @param value  the new value for the the record
-//    */
-//  def write (oid: Int, value: VDB.Record)
-//  {
-//    //obtain lock
-//    //VDB.write (tid, oid, value)
-//    //unlock before commit, so it should be implemented in commit
-//  } // write
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Begin this transaction.
-//    */
-//  def begin ()
-//  {
-//    VDB.begin (tid)
-//  } // begin
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Commit this transaction.
-//    */
-//  def commit ()
-//  {
-//
-//    println("commiting")
-//    for(i <- sch)
-//    {
-//      if(i._1==w)
-//        ul(i._3)
-//    }
-//    TransactionStats.count += 1
-//   // VDB.commit (tid)
-//    // if (DEBUG) println (VDB.logBuf)
-//  } // commit
-//
-//  //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-//  /** Rollback this transaction.
-//    */
-//  def rollback ()
-//  {
-//    VDB.rollback (tid)
-//  } // rollback
-//def searchlock(oid:Int,s:Schedule) : Boolean =
-//{
-//  var flag=false
-//  for(i <- s) {
-//    if (oid == i._3 && i._1 == 'w')
-//      flag=true
-//  }
-//  flag
-//}//to see if there is a writelock ahead
-//} // Transaction class
-//
-//
-////::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-///** The `TransactionTest` object is used to test the `Transaction` class.
-//  *  > run-main trans.TransactionTest
-//  */
-//object TransactionTest2 extends App
-//{
-//  // val t1=new Transaction(new Schedule(List((r,0,0))),2)//(r,0,1),(w, 0, 0), (w, 0, 1))),2)
-//  val startTime = System.currentTimeMillis()
-//  //val t1 = new Transaction (new Schedule (List ( (r, 0, 0), (r, 0, 1),(w, 0, 0), (w, 0, 1))),2)
-//  //val t2 = new Transaction (new Schedule (List ( (r, 1, 0), (r, 1, 1), (w, 1, 0), (w, 1, 1) )),2)
-//  //t1.start ()
-//  //t2.start ()
-//  var j =0
-// // for(i<-0 until 5) { // transactions*2----change this number to test the concurrent number of transactions
-//
-//   // j=i*2
-//
-//
-//    val t1 = new Transaction(new Schedule(List((r, j, 0), (r, j, 1), (w, j, 0), (w, j, 1), (r, j, 0), (r, j, 1))),2)
-//    // last 1 is for the tso ; 2 for 2pl
-//    val t2 = new Transaction(new Schedule(List( (w, j+1, 1))), 2) // last 1 is for the tso ; 2 for 2pl
-//
-//    t1.start()
-//    t2.start()
-//
-// // }
-//  Thread sleep 10000
-//  val endTime = System.currentTimeMillis()
-//  val totalTime=endTime-startTime
-//  println(TransactionStats.count+" time "+totalTime)
-//  val tps = TransactionStats.count.toDouble / (totalTime / 1000)
-//  println(s"tps=$tps")
-//} // TransactionTest object
-=======
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 /** @author  John Miller
@@ -243,6 +50,7 @@ import LockTable._
   */
 class Transaction (sch: Schedule,protocol: Int) extends Thread
 {
+  var rollbackid =false
   private val DEBUG = true              // debug flag
 private val tid = nextCount ()        // tarnsaction identifier
   val twoPl=2
@@ -252,34 +60,36 @@ private val tid = nextCount ()        // tarnsaction identifier
     */
   override def run ()
   {
-    begin ()
-    for (i <- sch) {
-      val op = i
-      val oid=i._3
-      println (i)
-      if (op._1 == r)
-        {
-          if(protocol==twoPl) {
-            if(searchlock(oid,sch)){
-              wl(oid,op._2)
+    do {
+      begin()
+      for (i <- sch) {
+        val op = i
+        val oid = i._3
+        println(i)
+        if (op._1 == r) {
+          if (protocol == twoPl) {
+            if (searchlock(oid, sch)) {
+              wl(oid, op._2, this)
               read(oid)
             }
             else {
-              rl(op._3,op._2)
+              rl(op._3, op._2, this)
               read(op._3)
-              lockmap(oid).lock.readLock().unlock() // ul(op._3)
+              // lockmap(oid).lock.readLock().unlock() // ul(op._3)
             }
           } //if
         } //if r/w
-      else {
-        if (protocol == twoPl) {
-          wl(op._3,op._2)
-          write(op._3, VDB.str2record(op.toString))
-        }
-      }//else of r/w
-    } // for
-    commit ()
+        else {
+          if (protocol == twoPl) {
+            wl(op._3, op._2, this)
+            write(op._3, VDB.str2record(op.toString))
+          }
+        } //else of r/w
+      } // for
+      commit()
+    } while(rollbackid)
   } // run
+
 
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   /** Read the record with the given 'oid'.
@@ -319,7 +129,9 @@ private val tid = nextCount ()        // tarnsaction identifier
     for(i <- sch)
       {
         if(i._1==w)
-        ul(i._3)
+        ul(w,i._3)
+        else
+          ul(r,i._3)
       }
    TransactionStats.count += 1
     VDB.commit (tid)
@@ -353,19 +165,35 @@ object TransactionTest extends App
 {
  // val t1=new Transaction(new Schedule(List((r,0,0))),2)//(r,0,1),(w, 0, 0), (w, 0, 1))),2)
   val startTime = System.currentTimeMillis()
-  var nTrans = 2
+  var nTrans = 10
   var nOps = 2
-  var nObjs = 2
-  //val t1 = new Transaction (new Schedule (List ( (r, 0, 0), (r, 0, 1),(w, 0, 0), (w, 0, 1))),2)
-  //val t2 = new Transaction (new Schedule (List ( (r, 1, 0), (r, 1, 1), (w, 1, 0), (w, 1, 1) )),2)
-  //t1.start ()
-  //t2.start ()
-  var transactions = Array.ofDim[Transaction](3)
-  for(i<-transactions.indices)
-    {
-      transactions(i)=new Transaction(Schedule.gen(i,nOps,nObjs),2)
-      transactions(i).start()
-    }
+  var nObjs = 32
+  /*val t1 = new Transaction (new Schedule (List ( (r, 0, 0), (r, 0, 1),(w, 0, 0), (w, 0, 1))),2)
+  val t2 = new Transaction (new Schedule (List ( (w, 1, 0), (r, 1, 1), (w, 1, 0), (w, 1, 1) )),2)
+  t1.start ()
+  t2.start ()
+  */
+  var transactions = Array.ofDim[Transaction](nTrans)
+  var j=0
+
+ /* for(i<-0 until 5) { // transactions*2----change this number to test the concurrent number of transactions
+
+    j=i*2
+
+    val t1 = new Transaction(new Schedule(List((r, j, 0), (r, j, 1), (w, j, 0), (w, j, 1), (r, j, 0), (r, j, 1))), 2)
+    // last 1 is for the tso ; 2 for 2pl
+    val t2 = new Transaction(new Schedule(List((r, j+1, 0), (r,j+1, 1), (w, j+1, 0), (w, j+1, 1))), 2) // last 1 is for the tso ; 2 for 2pl
+
+    t1.start()
+    t2.start()
+
+  }
+*/
+   for(i<-transactions.indices)
+     {
+       transactions(i)=new Transaction(Schedule.gen(i,nOps,nObjs),2)
+       transactions(i).start()
+     }
   Thread sleep 10000
   val endTime = System.currentTimeMillis()
   val totalTime=endTime-startTime
@@ -374,4 +202,3 @@ object TransactionTest extends App
   val tps =TransactionStats.count/(totalTime/1000)
   println(s"tps=$tps")
 } // TransactionTest object
->>>>>>> origin/master
