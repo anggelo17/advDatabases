8ab6054948101876a2a7d49f26fc83a6bf51e099
anggelo17 <anggelo17@gmail.com>
2017-02-25 14:34:47 -0500

    new p2l
    new p2l

--- src/main/scala/trans/LockTable.scala
+++ src/main/scala/trans/LockTable.scala
@@ -1,9 +1,14 @@
 package trans
 
+import java.lang.Exception
 import java.util.concurrent.locks.ReentrantReadWriteLock
+import javax.script.ScriptException
 
 import scala.collection.mutable
 import trans.TrafficLight._
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.control.Exception
 /**
   * Created by supriya on 2/9/17.
   */
@@ -11,118 +16,134 @@ object LockTable {
 class Lock(tid: Int) {
   val lock = new ReentrantReadWriteLock()
   var owner=tid
+  val transactionIds= new ArrayBuffer[Int]()
 }
   var lockmap = new mutable.HashMap[Int,Lock]()
-  val nTrans = 2
+  val nTrans = 5
   val waitgraph = new Graph(Array.fill(nTrans)(Set[Int]()))
 
-  def rl(oid: Int,tid: Int,trans: Transaction) =
+  def rl(oid: Int,tid: Int,trans: Transaction):Int =
   {
-    var ownerTid = -1
-    var wait =false
-    if(!(lockmap.keySet.contains(oid)))
+
+
+    if(!(lockmap.keySet.contains(oid))) {
       lockmap += (oid -> new Lock(tid))
-      //lockmap(oid).lock.readLock().lock()
 
-    if(tid != lockmap(oid).owner)
-      wait = true
-    ownerTid = lockmap(oid).owner
-    if(wait) {
+      lockmap(oid).lock.readLock().lock()
+      lockmap(oid).transactionIds += tid
+    }
+    else {
 
-      println("adding an edge from.."+tid+" to "+ownerTid+" when rl on"+oid)
-      waitgraph.ch(tid) += ownerTid}
+      if (lockmap(oid).lock.isWriteLocked){ // if writelocked by any thread
 
-    if(LockTable.checkCycle())
-      {
-        trans.rollbackid = true
-        if(wait) waitgraph.ch(tid) -= ownerTid
+        waitgraph.ch(tid) += lockmap(oid).owner  // edge from tid (read) to write(owner)
+        if(LockTable.checkCycle())
+        {
+          println("there is a cycle????")
+          waitgraph.printG()
+          trans.rollbackid = true
+          waitgraph.ch(tid) -= lockmap(oid).owner
+          return 1 //rollback
+        }
+        //do a wait until write-unlock on oid is unlocked.
+
       }
-    lockmap(oid).lock.readLock().lock() //tid got the readlock
-    println("after ..read locks on " + oid + " : " + lockmap(oid).lock.getReadLockCount + " " + lockmap(oid).lock.getReadHoldCount)
+
+      lockmap(oid).lock.readLock().lock()// this will wait
+      lockmap(oid).transactionIds += tid
 
 
-    //println("======="+lockmap(oid).lock.toString)
-  }
-  def wl(oid: Int,tid: Int,trans: Transaction) = {
-    var wait = false
-    var ownerTid = -1
-
-
-    println("starting wl on "+oid)
-
-    if (!lockmap.keySet.contains(oid)) {
-      println("inserting.."+oid)
-      lockmap += (oid) -> new Lock(tid)
     }
 
 
 
-    if(lockmap(oid).owner != tid)
-      wait = true
+    return 0//ok
 
-    ownerTid = lockmap(oid).owner
-    //println( " : " + lockmap(oid).lock.getReadLockCount + " " + lockmap(oid).lock.getReadHoldCount)
-    if(wait)
-      {
-        println("adding an edge from.."+tid+" to "+ownerTid+" when wl on"+oid)
-        waitgraph.ch(tid) += ownerTid
-      }
-    if(LockTable.checkCycle())
-      {
-        println("there is a cycle????")
-        waitgraph.printG()
-        trans.rollbackid=true
-        if(wait)
-        waitgraph.ch(tid) -= ownerTid
-      }
+    //println("after ..read locks on " + oid + " : " + lockmap(oid).lock.getReadLockCount + " " + lockmap(oid).lock.getReadHoldCount)
 
 
-
-      val readLockCount = lockmap(oid).lock.getReadLockCount
-      for (i <- 0 until readLockCount) {
-
-        println("ulocking.."+oid)
-
-        lockmap(oid).lock.readLock().unlock()
-      }
-
-
-
-    lockmap(oid).lock.writeLock().lock()
     //println("======="+lockmap(oid).lock.toString)
   }
-    def ul(op: Char,oid: Int,tid:Int): Unit = {
-      if(lockmap.keySet.contains(oid)) {
-        if (op=='w') {
-          println(s"unlocking writelock on $op, $oid")
+  def wl(oid: Int,tid: Int,trans: Transaction):Int  = {
 
-          lockmap(oid).lock.writeLock().synchronized {
-            waitgraph.ch(tid) -= lockmap(oid).owner
-            lockmap(oid).lock.writeLock().unlock()
-        }
 
-        }
-       /* else if(TransactionStats.upgradelock)
-          {
-            lockmap(oid).lock.writeLock().unlock()
-            TransactionStats.upgradelock=false
-          } */
-        else {
-         // println(s"unlocking readlock on $op, $oid")
+    println("starting wl on " + oid+" on thread id "+Thread.currentThread().getId)
 
-          waitgraph.ch(tid) -= lockmap(oid).owner
-          val readLockCount = lockmap(oid).lock.getReadLockCount
-          for (i <- 0 until readLockCount) {
+    if (!lockmap.keySet.contains(oid)) {
 
-            println("unlocking readlock in ul.."+oid)
+      lockmap += (oid) -> new Lock(tid)
+    }
 
-            lockmap(oid).lock.readLock().unlock()
-          }
 
-         // lockmap(oid).lock.readLock().unlock()
+    for(i <- 0 until lockmap(oid).transactionIds.size)// draw an edge from tid to  list of transactions ids for that oid ( from write to n reads/ 1 write )
+      waitgraph.ch(tid) += lockmap(oid).transactionIds(i)
 
-        }
+
+    if (LockTable.checkCycle()) {
+      println("there is a cycle????")
+      waitgraph.printG()
+      trans.rollbackid = true
+      waitgraph.ch(tid) -= lockmap(oid).owner
+      return 1// rollback
+    }
+
+
+    if (!lockmap(oid).lock.isWriteLocked)// if not writelocked by any thread
+      {
+        lockmap(oid).lock.writeLock().lock()
+        lockmap(oid).transactionIds +=  tid // should be just one
+
+        waitgraph.ch(tid) -= lockmap(oid).owner // once i got the lock erase the -> dependency from tid to owner.
+        lockmap(oid).owner = tid // update the new owner.
       }
+
+    else {//it is write-locked
+
+      // wait and try to lock after
+      println("here...wl")
+      lockmap(oid).lock.writeLock().lock()
+      lockmap(oid).transactionIds +=  tid // should be just one
+
+      waitgraph.ch(tid) -= lockmap(oid).owner // once i got the lock erase the -> dependency from tid to owner.
+      lockmap(oid).owner = tid
+
+    }
+
+    return 0// ok
+
+
+  }
+    def ul(op: Char,oid: Int,tid:Int): Unit = {
+
+      println("graph before unlock")
+      waitgraph.printG()
+
+      if (oid==1 && op=='w' && tid==1)
+        println("aqui")
+
+      if(lockmap.keySet.contains(oid)) {
+
+        if (lockmap(oid).lock.isWriteLockedByCurrentThread){
+
+          lockmap(oid).lock.writeLock().unlock()
+          lockmap(oid).transactionIds -= tid
+          waitgraph.ch(tid) -= lockmap(oid).owner
+        }
+
+        else { //else read unlock
+
+          lockmap(oid).lock.readLock().unlock()
+          lockmap(oid).transactionIds -= tid
+          waitgraph.ch(tid) -= lockmap(oid).owner
+
+        }
+
+
+      }
+
+      println("graph after unlock")
+      waitgraph.printG()
+
     }
    def checkCycle(): Boolean ={
      val color = Array.fill (waitgraph.size)(G_N)
--- src/main/scala/trans/Transaction2pl.scala
+++ src/main/scala/trans/Transaction2pl.scala
@@ -22,6 +22,8 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 
 import Operation._
 
+import scala.collection.mutable.ArrayBuffer
+
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 /** The `Transaction` companion object
   */
@@ -44,6 +46,14 @@ object Transaction
 
 } // Transaction object
 
+class OperationLock(Oper:Op,st:String)
+{
+  val ope=Oper
+  val str=st// 3-tuple (r/w, tid, oid)
+
+
+}
+
 import Transaction._
 import LockTable._
 
@@ -65,39 +75,80 @@ private val tid = nextCount ()        // tarnsaction identifier
   {
 
 
+      val oidList = new ArrayBuffer[OperationLock]()
       begin()
       val size = sch.sizeSchedule
       var i = 0;
-      while (i < size) {
+      var result=0;
+
+    if (protocol == twoPl) {
+
+      while (i < size) {// phase 1
         val op = sch(i)
-        println(op)
-        if (op._1 == r) {
-          if (protocol == twoPl) {
 
-              rl(op._3, op._2, this)
-              println(s"got readlock on $op")
+        if (!containsOpera(oidList,op._3)) {
+
+          if (op._1 == r) {// if read
+
+            if (searchlock(op._3, op._2, sch, i)) {
+              result=wl(op._3, op._2, this)
+              if(result==0) {
+                println(s"got writelock on $op")
+                val opelock = new OperationLock(op, "wl")
+                oidList += opelock
+              }
+            }
+            else {
+              result=rl(op._3, op._2, this)
+              if(result==0) {
+                println(s"got readlock on $op")
+                val opelock = new OperationLock(op, "rl")
+                oidList += opelock
+              }
+            }
 
 
-          } //if
-        } //if r/w
-        else {
-          if (protocol == twoPl) {
-            wl(op._3, op._2, this)
-            println(s"got writelock on $op")
+          }
+          else {
+
+            result=wl(op._3, op._2, this)
+            if(result==0) {
+              println(s"got writelock on $op")
+              val opelock = new OperationLock(op, "wl")
+              oidList += opelock
+            }
             //write(op._3, VDB.str2record(op.toString))
-          }
-        } //else of r/w
-        if(rollbackid)
-          {
-            i=0;
-            rollbackid=false
-            rollback()
-          }
-        else i +=1
+
+          }//else
+
+        }//if
+
+        if (rollbackid) {
+          i = 0;
+          rollbackid = false
+          rollback(oidList)
+        }
+        else i += 1
 
       } // while
-      commit()
 
+
+
+      i=0
+      while(i<size){
+        val ope=sch(i)
+
+        if (ope._1==r)
+          read(ope._3)
+        else
+          write(ope._3,VDB.str2record(ope.toString))
+
+        i += 1
+      }
+
+      commit(oidList)
+
+    }//if
   } // run
 
 
@@ -108,6 +159,7 @@ private val tid = nextCount ()        // tarnsaction identifier
   def read (oid: Int): VDB.Record =
   {
     //obtain lock. see if there is a write ahead, so the lock should be upgraded - searchlock
+    println("reading .."+oid)
     VDB.read (tid, oid)._1
   } // read
 
@@ -119,7 +171,8 @@ private val tid = nextCount ()        // tarnsaction identifier
   def write (oid: Int, value: VDB.Record)
   {
     //obtain lock
-    VDB.write (tid, oid, value)
+    println("writing .."+oid)
+    //VDB.write (tid, oid, value)
     //unlock before commit, so it should be implemented in commit
   } // write
 
@@ -134,16 +187,20 @@ private val tid = nextCount ()        // tarnsaction identifier
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   /** Commit this transaction.
     */
-  def commit ()
+  def commit (oidList:ArrayBuffer[OperationLock])
   {
-    for(i <- sch)
-      {
-        if(i._1==w)
-        ul(w,i._3,i._2)
-        else
-          ul(r,i._3,i._2)
-      }
-   TransactionStats.count += 1
+    var i=0
+    while (i < oidList.size)
+    {
+      if( oidList(i).str=="wl")
+        ul(w,oidList(i).ope._3,oidList(i).ope._2)
+      else
+        ul(r,oidList(i).ope._3,oidList(i).ope._2)
+
+      i += 1
+    }
+
+    TransactionStats.count += 1
     //VDB.commit (tid)
    // if (DEBUG) println (VDB.logBuf)
   } // commit
@@ -151,30 +208,45 @@ private val tid = nextCount ()        // tarnsaction identifier
   //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
   /** Rollback this transaction.
     */
-  def rollback ()
+  def rollback (oidList:ArrayBuffer[OperationLock])
   {
 
     println("rolling back")
-    for(i <- sch)
+    for(i <- oidList.indices)
     {
-      if(i._1==w)
-        ul(w,i._3,i._2)
-//      else
-//        ul(r,i._3)
+      if( oidList(i).str=="wl")
+        ul(w,oidList(i).ope._3,oidList(i).ope._2)
+      else
+        ul(r,oidList(i).ope._3,oidList(i).ope._2)
     }
+
+    TransactionStats.rolls +=1
     //VDB.rollback (tid)
   } // rollback
-/*def searchlock(oid:Int, tid: Int,s:Schedule, index: Int) : Boolean =
+def searchlock(oid:Int, tid: Int,s:Schedule, index: Int) : Boolean =
 {
   var flag=false
   for(i <- index+1 until s.sizeSchedule) {
-    if (oid == s(i)._3 && tid == s(i)._2 && s(i)._1 == 'w')
+    if (oid == s(i)._3 && s(i)._1 == 'w')
     flag=true
     println(s"in searchlock $i $flag")
 }
   flag
 }//to see if there is a writelock ahead
-*/
+
+  def containsOpera(lst:ArrayBuffer[OperationLock],oid:Int):Boolean=
+  {
+    var f=false
+
+    for( i <- lst.indices){
+      var op= lst(i).ope
+      if(op._3==oid && lst(i).str=="wl")
+        return true
+
+    }
+    f
+  }
+
 } // Transaction class
 
 
@@ -200,47 +272,42 @@ object TransactionTest extends App
 //  lock.writeLock().lock();  // Blocks as some thread (this one!) holds read lock
 //
 //  System.out.println("Will never get here");
- // val t1=new Transaction(new Schedule(List((r,0,0))),2)//(r,0,1),(w, 0, 0), (w, 0, 1))),2)
+
   val startTime = System.currentTimeMillis()
-  var nTrans = 2
-  var nOps = 2
-  var nObjs = 2
+  var nTrans = 5
+  var nOps = 4
+  var nObjs = 10
   /*val t1 = new Transaction (new Schedule (List ( (r, 0, 0), (r, 0, 1),(w, 0, 0), (w, 0, 1))),2)
   val t2 = new Transaction (new Schedule (List ( (w, 1, 0), (r, 1, 1), (w, 1, 0), (w, 1, 1) )),2)
   t1.start ()
   t2.start ()
   */
   var transactions = Array.ofDim[Transaction](nTrans)
-  var j,i=0
 
- // for(i<-0 until 5) { // transactions*2----change this number to test the concurrent number of transactions
 
-    j=i*2
-
-    val t1 = new Transaction(new Schedule(List( (r, j, 1), (r, j, 1), (r, j, 0), (w, j, 0))), 2)
-    // last 1 is for the tso ; 2 for 2pl
-    val t2 = new Transaction(new Schedule(List((w, j+1, 1), (r,j+1, 1), (w, j+1, 0), (w, j+1, 1))), 2) // last 1 is for the tso ; 2 for 2pl
-
-    t1.start()
-    t2.start()
+//  var j,i=0
+//
+//    val t1 = new Transaction(new Schedule(List( (r, j, 1), (r, j, 1), (r, j, 0), (w, j, 0),(r,0,0))), 2)
+//    // last 1 is for the tso ; 2 for 2pl
+//    val t2 = new Transaction(new Schedule(List((w, j+1, 1), (r,j+1, 1), (w, j+1, 0), (w, j+1, 1))), 2) // last 1 is for the tso ; 2 for 2pl
+//
+//    t1.start()
+//    t2.start()
 
 
 
+   for(i<-transactions.indices)
+     {
+       transactions(i)=new Transaction(Schedule.gen(i,nOps,nObjs),2)
+       transactions(i).start()
+     }
 
 
-
- // }
-
-//   for(i<-transactions.indices)
-//     {
-//       transactions(i)=new Transaction(Schedule.gen(i,nOps,nObjs),2)
-//       transactions(i).start()
-//     }
-  Thread sleep 10000
-  val endTime = System.currentTimeMillis()
-  val totalTime=endTime-startTime
-  println("Committed="+ TransactionStats.count)
-  println(s"totaltime=$totalTime")
-  val tps =TransactionStats.count/(totalTime/1000)
-  println(s"tps=$tps")
+//  Thread sleep 10000
+//  val endTime = System.currentTimeMillis()
+//  val totalTime=endTime-startTime
+//  println("Committed="+ TransactionStats.count)
+//  println(s"totaltime=$totalTime")
+//  val tps =TransactionStats.count/(totalTime/1000)
+//  println(s"tps=$tps")
 } // TransactionTest object
